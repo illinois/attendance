93e55d8d20e1415cdf4bf32b61388b6a3bc741ef
Kevin Wang <kevin@kevinwang.com>
2014-08-26 15:16:52 -0500

    Update i-card regex for the production card reader format.

--- parse-swipe.js
+++ parse-swipe.js
@@ -4,7 +4,7 @@
  * Returns null if swipe data is invalid.
  */
 var parseSwipe = function(swipeData) {
-    var re = /;6397(\d{9})\d{3}=\d{11}\?/;
+    var re = /6397(\d{9})\d{3}/;
     var result = re.exec(swipeData);
     return result ? result[1] : null;
 };
