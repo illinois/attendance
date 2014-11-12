022fb219a3217309b0501af8192286ea10ea98c3
Kevin Wang <kevin@kevinwang.com>
2014-11-11 21:19:15 -0600

    Add Markdown support to comments.

--- client/comment-section.jsx
+++ client/comment-section.jsx
@@ -3,6 +3,8 @@
 var React = require('react/addons');
 var $ = require('jquery');
 var moment = require('moment');
+var marked = require('marked');
+marked.setOptions({sanitize: true});
 
 var baseUrl = require('../baseurl');
 
@@ -36,6 +38,11 @@ var CommentSection = React.createClass({
 
     handleCommentTextChange: function(e) {
         this.setState({commentText: e.target.value});
+
+        // Scroll to bottom of page after preview renders
+        setTimeout(function() {
+            window.scrollTo(0, document.body.scrollHeight);
+        }, 0);
     },
 
     handleSubmit: function() {
@@ -76,6 +83,7 @@ var CommentSection = React.createClass({
 
         var comments = this.state.comments.map(function(comment) {
             var time = moment(comment.createdAt).calendar();
+            var textHTML = marked(comment.text);
 
             // TODO(klwang3): Remove inline styling
             return <div key={comment.id} style={{paddingBottom: '20px'}}>
@@ -84,14 +92,24 @@ var CommentSection = React.createClass({
                     {' '}
                     <span style={{color: '#aaa'}}>{time}</span>
                 </div>
-                <div>{comment.text}</div>
+                <div dangerouslySetInnerHTML={{__html: textHTML}} />
             </div>;
         });
+
+        var preview = null;
+        if (this.state.commentText) {
+            previewHTML = marked(this.state.commentText);
+            preview = <div>
+                <h3>Preview</h3>
+                <div dangerouslySetInnerHTML={{__html: previewHTML}} />
+            </div>;
+        }
 
         return <div className="row">
             <div className="col-md-6">
                 <h2>Comments</h2>
                 {comments}
+                {preview}
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <textarea
